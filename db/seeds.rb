ddca9ea8a50c8dc81bf8c8ef552bbca14b6faa51
willieLOH <willie.loh@live.com>
2015-11-02 15:24:54 +0800

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ User.create!(name:  "Example User",
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name,
+  User.create!(name: name,
               email: email,
               password:              password,
               password_confirmation: password,
@@ -26,8 +26,17 @@ User.create!(name:  "Example User",
               activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
